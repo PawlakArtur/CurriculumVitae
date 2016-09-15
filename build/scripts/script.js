22599e11536361dbb7f2d34b850ad2c9c42466dc
Artur Pawlak <pawlak.artur90@gmail.com>
2016-09-15 19:56:03 +0200

    add change style of navbar on scroll and change colors

--- build/scripts/script.js
+++ build/scripts/script.js
@@ -1,1 +1,12 @@
-console.log("test");
+window.addEventListener("DOMContentLoaded", function() {
+    var scrollpos = window.scrollY;
+    var nav = document.getElementsByTagName('nav');
+    window.addEventListener('scroll', function() {
+        scrollpos = window.scrollY;
+        if(scrollpos > 40) {
+            nav[0].classList.toggle('navBarSmall', true);
+        } else {
+            nav[0].classList.toggle('navBarSmall', false);
+        }
+    })
+}, false);
